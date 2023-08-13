cf76eb27909b8405e8d04aad5d63e9d13f95b644
maala-dxb <maala.m.mhrez@gmail.com>
2023-08-13 14:26:30 +0400

    skip prompt in the dockerfile

--- setup.sh
+++ setup.sh
@@ -6,32 +6,8 @@ set -e
 platform=`uname`
 linux_pro=`uname -m`
 
-
-while getopts ":f:" opt; do
-  case $opt in
-    f) command_from="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    exit 1
-    ;;
-  esac
-
-  case $OPTARG in
-    -*) echo "Option $opt needs a valid argument"
-    exit 1
-    ;;
-  esac
-done
-
-echo "command from $command_from"
-
-# skip those steps if the script is used within docker file
-if [[ "$command_from" == "dockerfile" ]]; then
-  echo "skipping dev install for dockerfile.."
-else
-  sudo apt install yq
-  sudo cp dev /usr/local/bin/dev
-fi
+sudo apt install -y yq
+sudo cp dev /usr/local/bin/dev
 
 
 if [[ $platform != "Linux" ]]; then
@@ -42,11 +18,11 @@ fi
 
 if [[ "$linux_pro" == "x86_64" ]]; then # amd machine
 	curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
-        sudo install skaffold /usr/local/bin/
+        sudo install -y skaffold /usr/local/bin/
 
 elif [[ "$linux_pro" == "arm" ]]; then # arm machine
 	curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-arm64 && \
-        sudo install skaffold /usr/local/bin/
+        sudo install -y skaffold /usr/local/bin/
 
 else
 	echo "Oops unknown processor $linux_pro, only linux is supported for now!"
